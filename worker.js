cb73759bf23b6d4082763021cd5958c625d7d523
Leonardo Russo <info@lrusso.com>
2022-06-05 01:42:18 -0300

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,35 @@
+const filesToCache = [
+	"Nintendo64.htm",
+	"Nintendo64.json",
+	"Nintendo64.png",
+	"Nintendo64FavIcon_16x16.png",
+	"Nintendo64FavIcon_192x192.png",
+	"Nintendo64FavIcon_512x512.png",
+	"Nintendo64Game.htm",
+	"Nintendo64Game.js",
+	"Nintendo64Share.png"
+];
+
+const staticCacheName = "nintendo64-v1";
+
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(staticCacheName)
+		.then(cache => {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request)
+		}).catch(error => {
+		})
+	);
+});
